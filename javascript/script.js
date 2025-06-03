46b4f51bca469785dc3d600edb4cefcdb3bf922c
Lucas <lucas_r_kormann@estudante.sesisenai.org.br>
2025-06-03 08:32:07 -0300

    Update - Criação de tela de usuário para personalização de informações com validação em JS, atualização do botão de perfil.

--- javascript/script.js
+++ javascript/script.js
@@ -77,3 +77,63 @@ function validarFormulario() { //Validação de email do login
 function redirecionar_bem_vindo() { //Função para redirecionar o botão de login para a página de bem vindo
     window.location.href = 'bem_vindo.html';
 }
+
+function redirecionar_usuario() {
+    window.location.href = 'tela_usuario.html';
+}
+
+var nomeCompleto = document.getElementById('nomeCompleto');
+var email = document.getElementById('email');
+var telefone = document.getElementById('telefone');
+var dataNascimento = document.getElementById('dataNascimento');
+
+let valid = true;
+
+if (nomeCompleto.value.trim() === '') {
+    alert('Por favor, preencha o nome do cliente');
+    valid = false;
+}
+
+var telefoneRegex = /^\d{11}$/;
+if (!telefoneRegex.test(telefone.value)) {
+    alert('Telefone deve conter 11 dígitos (DDD + número)');
+    valid = false;
+}
+
+if (email.value !== '') {
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email.value)) {
+        alert('Por favor, insira um e-mail válido');
+        valid = false;
+    }
+}
+
+function validadataNascimento() {
+    var dataNascimento = document.getElementById("dataNascimento").value;
+    if (!dataNascimento) {
+        alert('Por favor, preencha a data de nascimento');
+        return false;
+    }
+
+    dataNascimento = dataNascimento.replace(/\//g, "-"); 
+    var dataNascimento_array = dataNascimento.split("-"); 
+            
+    if(dataNascimento_array[0].length != 4){
+       dataNascimento = dataNascimento_array[2]+"-"+dataNascimento_array[1]+"-"+dataNascimento_array[0]; 
+    }
+
+    var hoje = new Date();
+    var nasc  = new Date(dataNascimento);
+    var idade = hoje.getFullYear() - nasc.getFullYear();
+    var m = hoje.getMonth() - nasc.getMonth();
+    if (m < 0 || (m === 0 && hoje.getDate() < nasc.getDate())) idade--;
+    
+    if(idade < 18){
+       alert("Insira uma data válida (maior que 18 anos).");
+       return false;
+    }
+    
+    return true;
+}
+
+validadataNascimento();
